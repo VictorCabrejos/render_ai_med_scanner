59bfa32b966e079e090087c39de4120b0681ed61
Victor Cabrejos <thevictor23@gmail.com>
2019-12-06 03:38:56 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=16VBqRaamj2mnbs28kunRj8qtGYTetOyZ'
+export_file_url = 'https://drive.google.com/uc?id=16VBqRaamj2mnbs28kunRj8qtGYTetOyZ'
 export_file_name = 'model.pth'
 
 classes = ['NORMAL', 'PNEUMONIA']
